9041c823bf6923bf44700c46dafa79baef3b8b13
ssum-dal <min8226@gmail.com>
2021-11-27 01:04:14 +0900

    [완전탐색] 프로그래머스 고득점 Kit

---
+++ 프로그래머스/고득점키트/완전탐색/카펫.py
@@ -0,0 +1,14 @@
+def solution(brown, yellow):
+    answer = []
+    w = 0
+    
+    print(yellow//2+1 )
+    for i in range(1, yellow//2+1):
+        if yellow % i == 0:
+            w = yellow // i
+            if 2*(w+2) + i*2 == brown:
+                answer = [w+2, i+2]
+                break
+    return answer
+
+solution(8, 1)
