cea650cef09f25d1703e344c2ef3ff344405a545
ssum-dal <min8226@gmail.com>
2021-12-18 21:33:20 +0900

    [REFACTOR] 코드 수정

--- 프로그래머스/고득점키트/정렬/가장큰수.py
+++ 프로그래머스/고득점키트/정렬/가장큰수.py
@@ -1,13 +1,18 @@
 def solution(numbers):
     answer = ''
     
-    for i in range(len(numbers)):
-        numbers[i] = str(numbers[i])*4
+    #for i in range(len(numbers)):
+    #    numbers[i] = str(numbers[i])*3
+    #numbers.sort(reverse = True)
+
+    #이렇게 바꾸기
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
     
-    numbers.sort(reverse = True)
-    
-    for i in numbers:
-        answer = answer + i[0:len(i)//4]
+    #for i in numbers:
+    #    answer = answer + i[0:len(i)//4]
+
+    answer = str(int(''.join(numbers)))
     
     return answer
 
